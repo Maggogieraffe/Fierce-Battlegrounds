3dc3890b90cd312c4f935526a0ea022e2276f228
Raphael220284 <151544264+Maggogieraffe@users.noreply.github.com>
2024-09-05 12:16:36 +0200

    e

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Cam : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CinemachineVirtualCamera cam;
+    CinemachineComponentBase componentBase;
     void Start()
     {
-        
+        cam = GetComponent<CinemachineVirtualCamera>();
+        componentBase = cam.GetCinemachineComponent(CinemachineCore.Stage.Body);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 bal = Input.mouseScrollDelta;
+        print(bal);
+        if (componentBase is CinemachineFramingTransposer)
+        {
+            (componentBase as CinemachineFramingTransposer).m_CameraDistance += bal.y; //CameraZoomDistance
+        }
     }
 }
